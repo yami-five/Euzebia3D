7f31cbceaa12c6fea011612ee34bf641dfe10d39
yami-five <konrad.rykala.94@gmail.com>
2026-06-29 10:07:31 +0200

    expanded sprites size

--- libs/storage/sprites.h
+++ libs/storage/sprites.h
@@ -8,8 +8,8 @@
 typedef struct
 {
     const uint16_t *pixels;
-    const uint8_t height;
-    const uint8_t width;
+    const uint16_t height;
+    const uint16_t width;
     const bool canRotate;
 } Sprite;
 
