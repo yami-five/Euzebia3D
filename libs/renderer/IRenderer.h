4c901919b172438b8aef7abd547283c83c2029b9
yami-five <konrad.rykala.94@gmail.com>
2026-07-18 12:10:42 +0200

    implemented primitives in renderer

--- libs/renderer/IRenderer.h
+++ libs/renderer/IRenderer.h
@@ -7,11 +7,14 @@
 #include "mesh.h"
 #include "light.h"
 #include "camera.h"
+#include "primitives.h"
 
 typedef struct
 {
     void (*init_renderer)(const IHardware * hardware, const IPainter * painter);
     void (*add_model_to_scene)(Mesh *mesh, Camera *camera, Light *light);
+    void (*add_point_to_scene)(Point3D *point, Camera *camera);
+    void (*add_line_to_scene)(Line3D *line, Camera *camera);
     void (*clean_scene)();
     void (*render_scene)(Light *light);
     void (*set_scale)(uint8_t scale);
--- libs/renderer/renderer.h
+++ libs/renderer/renderer.h
@@ -3,43 +3,6 @@
 
 #include "IRenderer.h"
 #include "fpa.h"
-#include "camera.h"
-#include "light.h"
-#include "material.h"
-#include "mesh.h"
-#include "string.h"
-
-typedef struct
-{
-    Vector4 a;
-    Vector4 b;
-    Vector4 c;
-    Vector2 uvA;
-    Vector2 uvB;
-    Vector2 uvC;
-} TriangleToRender;
-
-typedef struct
-{
-    Vector2 a;
-    Vector2 b;
-    Vector2 c;
-} Triangle2D;
-
-typedef struct
-{
-    Vector3 a;
-    Vector3 b;
-    Vector3 c;
-} Triangle3D;
-
-typedef struct
-{
-    Triangle3D TriangleOnScreen;
-    Triangle2D UV;
-    int32_t LightDistances[3];
-    Material *mat;
-} TriangleInScene;
 
 const IRenderer *get_renderer(void);
 void renderer_set_scale(uint8_t scale);
